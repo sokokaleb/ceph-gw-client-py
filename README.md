dc7966b4a4124f266b7a795da11970d5d301eb7b
Pusaka Kaleb Setyabudi <sokokaleb@gmail.com>
2016-12-24 04:42:20 +0700

    Add .env and app.run() support.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ceph-gw-client-py
+
+Remember, everytime a (key, value) is added to `.env`, copy it to `.env.example` too.
--- client.py
+++ client.py
@@ -1,1 +1,17 @@
 from flask import Flask
+from flask_dotenv import DotEnv
+app = Flask(__name__)
+env = DotEnv()
+env.init_app(app)
+env.eval(keys={
+    'DEBUG': int
+});
+
+# Anthony please continue here below
+# Cek konten dari .env, cara pakenya tinggal app.config['KEY']
+
+# Supaya ngga usah pake command `flask run`, tinggal `python client.py` aja.
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',
+            port=app.config['PORT'],
+            debug=app.config['DEBUG'])
