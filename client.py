9af22cef44e7df75911016d3e6e479309d9807d5
Anthony <thonywong@rocketmail.com>
2016-12-23 12:31:43 +0700

    Add temporary files to list and create bucket

--- client.py
+++ client.py
@@ -1,1 +1,21 @@
 from flask import Flask
+import rados, sys
+
+app = Flask(__name__)
+cluster = rados.Rados(conffile='/etc/ceph/ceph.conf', conf=dict(keyring='/etc/ceph/ceph.client.admin.keyring'))
+
+# get list of buckets
+@app.route('/', methods=['GET'])
+def bucket_get():
+    pools = cluster.list_pools()
+    results = ''
+    for pool in pools:
+        results += pool
+    return jsonify('{result: pool, meta: {status_code: 200, message: "OK"}}')
+
+# create a new bucket
+@app.route('/<bucket_name>', methods=['PUT'])
+def bucket_create(bucket_name):
+    cluster.create_pool('CUSTOM_REST_API_BUCKET-' + bucket_name)
+    return jsonify('{meta: {status_code: 200, message: "OK"}}')
+
