4ab54fae44c190149bae57dab7ccef740390dd46
Anthony <thonywong@rocketmail.com>
2016-12-24 06:59:18 +0700

    Rebase with origin master

--- client.py
+++ client.py
@@ -10,9 +10,6 @@ env.eval(keys={
     'DEBUG': int
 });
 
-# Anthony please continue here below
-# Cek konten dari .env, cara pakenya tinggal app.config['KEY']
-
 cluster = rados.Rados(conffile='/etc/ceph/ceph.conf', conf=dict(keyring='/etc/ceph/ceph.client.admin.keyring'))
 
 # get list of buckets
@@ -21,14 +18,36 @@ def bucket_get():
     pools = cluster.list_pools()
     results = []
     for pool in pools:
-        results.append(pool)
+        if app.config['BUCKET_PREFIX'] in pool:
+            results.append(pool)
     return jsonify({'result': results, 'meta': {'status_code': 200, 'message': 'OK'}})
 
 # create a new bucket
 @app.route('/<bucket_name>', methods=['PUT'])
 def bucket_create(bucket_name):
-    cluster.create_pool(app.config['BUCKET_PREFIX'] + bucket_name)
+    cluster.create_pool(app.config['BUCKET_PREFIX'] + '-' + bucket_name)
     return jsonify({'meta': {'status_code': 200, 'message': 'OK'}})
+
+# delete a specific bucket
+@app.route('/<bucket_name>', methods=['DELETE'])
+def bucket_delete(bucket_name):
+    # TODO: add an exception when there is no such bucket_name
+    cluster.delete_pool(app.config['BUCKET_PREFIX'] + '-' + bucket_name)
+    return jsonify({'meta': {'status_code': 200, 'message': 'OK'}})
+
+# list all objects in a specific bucket
+@app.route('/<bucket_name>', methods=['GET'])
+def bucket_list(bucket_name):
+    ioctx = cluster.open_ioctx(app.config['BUCKET_PREFIX'] + '-' + bucket_name)
+    results = []
+    # TODO: add an exception when there is no such bucket_name
+    while True:
+        try:
+            rados_object = object_iterator.next()
+            results.append(rados_object)
+        except StopIteration:
+            break
+    return jsonify({'result': results, 'meta': {'status_code' 200, 'message': 'OK'}})
 
 cluster.connect()
 
