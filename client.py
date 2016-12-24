d1371fec9761d3baef23daffc38a7577dfa981d4
Anthony <thonywong@rocketmail.com>
2016-12-24 09:37:47 +0700

    Use decorator

--- client.py
+++ client.py
@@ -10,12 +10,20 @@ env.eval(keys={
     'DEBUG': int
 });
 
-cluster = rados.Rados(conffile='/etc/ceph/ceph.conf', conf=dict(keyring='/etc/ceph/ceph.client.admin.keyring'))
+def append_cluster(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        cluster = rados.Rados(conffile=app.config['CONF_FILE'], conf=dict(keyring=app.config['KEYRING']))
+        cluster.connect()
+        return f(cluster=cluster, *args, **kwargs)
+    return decorated_function
 
 # get list of buckets
 @app.route('/', methods=['GET'])
+@append_cluster
 def bucket_get():
     pools = cluster.list_pools()
+    cluster.shutdown()
     results = []
     for pool in pools:
         if app.config['BUCKET_PREFIX'] in pool:
@@ -24,32 +32,50 @@ def bucket_get():
 
 # create a new bucket
 @app.route('/<bucket_name>', methods=['PUT'])
+@append_cluster
 def bucket_create(bucket_name):
     cluster.create_pool(app.config['BUCKET_PREFIX'] + '-' + bucket_name)
+    cluster.shutdown()
     return jsonify({'meta': {'status_code': 200, 'message': 'OK'}})
 
 # delete a specific bucket
 @app.route('/<bucket_name>', methods=['DELETE'])
+@append_cluster
 def bucket_delete(bucket_name):
     # TODO: add an exception when there is no such bucket_name
     cluster.delete_pool(app.config['BUCKET_PREFIX'] + '-' + bucket_name)
+    cluster.shutdown()
+    return jsonify({'meta': {'status_code': 200, 'message': 'OK'}})
+
+# add a new object to an existing cluster
+@app.route('/<bucket_name>/<object_name>', methods=['PUT'])
+@append_cluster
+def object_put(bucket_name, object_name):
+    # TODO: add an exception when there is no such bucket_name
+    ioctx = cluster.open_ioctx(bucket_name)
+    # TODO: change to value in request payload
+    ioctx.write(object_name, 'temporary value')
+    ioctx.close()
+    cluster.shutdown()
     return jsonify({'meta': {'status_code': 200, 'message': 'OK'}})
 
 # list all objects in a specific bucket
 @app.route('/<bucket_name>', methods=['GET'])
+@append_cluster
 def bucket_list(bucket_name):
-    ioctx = cluster.open_ioctx(app.config['BUCKET_PREFIX'] + '-' + bucket_name)
-    results = []
     # TODO: add an exception when there is no such bucket_name
+    ioctx = cluster.open_ioctx(app.config['BUCKET_PREFIX'] + '-' + bucket_name)
+    object_iterator = ioctx.list_objects()
+    results = []
     while True:
         try:
             rados_object = object_iterator.next()
             results.append(rados_object)
         except StopIteration:
             break
+    ioctx.close()
+    cluster.shutdown()
     return jsonify({'result': results, 'meta': {'status_code': 200, 'message': 'OK'}})
-
-cluster.connect()
 
 # Supaya ngga usah pake command `flask run`, tinggal `python client.py` aja.
 if __name__ == '__main__':
