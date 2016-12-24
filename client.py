fbb921c77d03a07ead5a0d609d73a734bb18ff0e
Pusaka Kaleb Setyabudi <sokokaleb@gmail.com>
2016-12-24 18:33:18 +0700

    Refactor and resolve todos

--- client.py
+++ client.py
@@ -18,8 +18,14 @@ def append_cluster(f):
     def decorated_function(*args, **kwargs):
         cluster = rados.Rados(conffile=app.config['CONFFILE_PATH'], conf=dict(keyring=app.config['KEYRING_PATH']))
         cluster.connect()
-        return f(cluster=cluster, *args, **kwargs)
+        res = f(cluster=cluster, *args, **kwargs)
+        cluster.shutdown()
+        return res
     return decorated_function
+
+# helper to get prefixed bucket name
+def get_bucket_name(bucket_name):
+    return '{}-{}'.format(app.config['BUCKET_PREFIX'], bucket_name)
 
 # get list of buckets
 @app.route('/', methods=['GET'])
@@ -31,33 +37,29 @@ def bucket_get(cluster):
     for pool in pools:
         if app.config['BUCKET_PREFIX'] in pool:
             results.append(pool)
-    return (jsonify(results), 200)
+    return jsonify(results), 200
 
 # create a new bucket
 @app.route('/<bucket_name>', methods=['PUT'])
 @append_cluster
 def bucket_create(cluster, bucket_name):
-    bucket_name = app.config['BUCKET_PREFIX'] + '-' + bucket_name
+    bucket_name = get_bucket_name(bucket_name)
     if cluster.pool_exists(bucket_name):
-        cluster.shutdown()
-        return ('bucket ' + bucket_name + ' exists', 309)
+        return 'bucket {} exists'.format(bucket_name), 309
     else:
         cluster.create_pool(bucket_name)
-        cluster.shutdown()
-        return ('', 200)
+        return '', 200
 
 # delete a specific bucket
 @app.route('/<bucket_name>', methods=['DELETE'])
 @append_cluster
 def bucket_delete(cluster, bucket_name):
-    bucket_name = app.config['BUCKET_PREFIX'] + '-' + bucket_name
+    bucket_name = get_bucket_name(bucket_name)
     if cluster.pool_exists(bucket_name):
         cluster.delete_pool(bucket_name)
-        cluster.shutdown()
-        return ('bucket ' + bucket_name + ' deleted', 200)
+        return 'bucket {} deleted'.format(bucket_name), 200
     else:
-        cluster.shutdown()
-        return ('bucket ' + bucket_name + ' not found', 404)
+        return 'bucket {} not found'.format(bucket_name), 404
 
 # helper function to get an object
 def get_object_content(cluster, bucket_name, object_name):
@@ -75,85 +77,76 @@ def get_object_content(cluster, bucket_name, object_name):
 @app.route('/<bucket_name>/<object_name>', methods=['PUT'])
 @append_cluster
 def object_put(cluster, bucket_name, object_name):
-    bucket_name = app.config['BUCKET_PREFIX'] + '-' + bucket_name
+    bucket_name = get_bucket_name(bucket_name)
     if cluster.pool_exists(bucket_name):
         try:
             ioctx = cluster.open_ioctx(bucket_name)
             ioctx.write_full(object_name, request.data)
             ioctx.close()
-            cluster.shutdown()
-            return ('created new object ' + bucket_name + '/' + object_name, 200)
+            return 'created new object {}/{}'.format(bucket_name,  object_name), 200
         except Exception:
-            cluster.shutdown()
-            return ('error writing object', 406)
+            return 'error writing object', 406
     else:
-        cluster.shutdown()
-        return ('bucket ' + bucket_name + ' not found', 404)
+        return 'bucket {} not found'.format(bucket_name), 404
 
 # get the content of an object
 @app.route('/<bucket_name>/<object_name>', methods=['GET'])
 @append_cluster
 def object_get(cluster, bucket_name, object_name):
-    bucket_name = app.config['BUCKET_PREFIX'] + '-' + bucket_name
+    bucket_name = get_bucket_name(bucket_name)
     if cluster.pool_exists(bucket_name):
         ioctx = cluster.open_ioctx(bucket_name)
         size, _ = ioctx.stat(object_name)
         result = ioctx.read(object_name, size)
         ioctx.close()
-        cluster.shutdown()
-        return (result, 200, {'Content-Disposition': 'attachment; filename=%s' % object_name})
+        return result, 200, {'Content-Disposition': 'attachment; filename=%s' % object_name}
     else:
-        cluster.shutdown()
-        return ('bucket ' + bucket_name + ' not found', 404)
+        return 'bucket {} not found'.format(bucket_name), 404
 
 # delete an object from a bucket
 @app.route('/<bucket_name>/<object_name>', methods=['DELETE'])
 @append_cluster
 def object_delete(cluster, bucket_name, object_name):
-    bucket_name = app.config['BUCKET_PREFIX'] + '-' + bucket_name
+    bucket_name = get_bucket_name(bucket_name)
     if cluster.pool_exists(bucket_name):
         ioctx = cluster.open_ioctx(bucket_name)
         try:
             ioctx.remove_object(object_name)
-            ioctx.close()
-            cluster.shutdown()
-            return ('', 200)
+            return '', 200
         except Exception:
+            return 'object {} not found'.format(object_name), 404
+        finally:
             ioctx.close()
-            cluster.shutdown()
-            return ('object ' + object_name + ' not found', 404)
     else:
-        return ('bucket ' + bucket_name + ' not found', 404)
+        return 'bucket {} not found'.format(bucket_name), 404
 
 # copy an object
 @app.route('/<source_bucket>/<source_object>/<dest_bucket>/<dest_object>', methods=['COPY'])
 @append_cluster
 def object_copy(cluster, source_bucket, source_object, dest_bucket, dest_object):
-    source_bucket = app.config['BUCKET_PREFIX'] + '-' + source_bucket
-    dest_bucket = app.config['BUCKET_PREFIX'] + '-' + dest_bucket
+    source_bucket = get_bucket_name(source_bucket)
+    dest_bucket = get_bucket_name(dest_bucket)
     source_object_ref = get_object_content(cluster, source_bucket, source_object)
     dest_object_ref = get_object_content(cluster, dest_bucket, dest_object)
     if source_object_ref is None:
-        cluster.shutdown()
-        return ('source object not found', 404)
+        return 'source object not found', 404
     if dest_object_ref is not None:
-        cluster.shutdown()
-        return ('target exist', 409)
+        return 'target exist', 409
     if not cluster.pool_exists(dest_bucket):
-        cluster.shutdown()
-        return ('destination bucket not found', 404)
+        return 'destination bucket not found', 404
     ioctx = cluster.open_ioctx(dest_bucket)
     ioctx.write_full(dest_object, source_object_ref)
     ioctx.close()
-    cluster.shutdown()
-    return ('copied', 200)
+    return 'copied', 200
 
 # list all objects in a specific bucket
 @app.route('/<bucket_name>', methods=['GET'])
 @append_cluster
 def bucket_list(cluster, bucket_name):
-    # TODO: add an exception when there is no such bucket_name
-    ioctx = cluster.open_ioctx(app.config['BUCKET_PREFIX'] + '-' + bucket_name)
+    bucket_name = get_bucket_name(bucket_name)
+    if not cluster.pool_exists(bucket_name):
+        return 'bucket {} not found'.format(bucket_name), 404
+    ioctx = cluster.open_ioctx(bucket_name)
     object_iterator = ioctx.list_objects()
     results = []
     while True:
@@ -163,8 +156,7 @@ def bucket_list(cluster, bucket_name):
         except StopIteration:
             break
     ioctx.close()
-    cluster.shutdown()
-    return (jsonify(results), 200)
+    return jsonify(results), 200
 
 # Supaya ngga usah pake command `flask run`, tinggal `python client.py` aja.
 if __name__ == '__main__':
